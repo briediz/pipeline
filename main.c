772545c309f9e0050959a12543e1d7cc139f507f
Dai Ram <dainius@gmx.net>
2023-10-03 15:40:07 +0200

    adding somethin on main

--- main.c
+++ main.c
@@ -2,3 +2,7 @@ do some coding on main branch
 do some coding on feature branch
 do some coding on feature branch
 
+
+
+main added something in main branch
+
---
+++ main.h
@@ -0,0 +1,1 @@
+added something to main head file
